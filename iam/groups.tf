feb0427c924c9b9db4613f798e4a07cf08070941
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-02-07 23:35:18 -0800

    clean-up

--- iam/groups.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_user_group_membership" "user_group_membership" {
-  for_each = var.users
-
-  user = aws_iam_user.user[each.key].name
-  groups = each.value.groups
-}
--- iam/users.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_user" "user" {
-  for_each = var.users
-
-  name = each.key
-}
